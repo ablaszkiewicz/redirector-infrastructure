dd33a96906a4a58265fbe7f7320b120a1bac7fa8
Aleksander Blaszkiewicz <kqmdjc8@gmail.com>
2023-04-30 20:15:45 +0200

    Modify location matching

--- reverse-proxy/src/nxconf.sh
+++ reverse-proxy/src/nxconf.sh
@@ -34,6 +34,24 @@ server {
         proxy_connect_timeout 120;
     }
 
+    location = / {
+        proxy_pass http://192.168.3.130:3003;
+        proxy_set_header Host \$host;
+        proxy_http_version 1.1;
+        proxy_set_header Upgrade \$http_upgrade;
+        proxy_set_header Connection \$connection_upgrade;
+        proxy_ssl_name \$host;
+        proxy_ssl_server_name on;
+        proxy_ssl_verify off;
+        proxy_ssl_protocols  TLSv1 TLSv1.1 TLSv1.2;
+        proxy_ssl_session_reuse off;
+        proxy_set_header X-Forwarded-For \$remote_addr;
+        proxy_set_header X-Forwarded-Proto \$scheme;
+        proxy_read_timeout 120;
+        proxy_send_timeout 120;
+        proxy_connect_timeout 120;
+    }
+
     location / {
         proxy_pass http://192.168.3.130:3001;
         proxy_set_header Host \$host;
