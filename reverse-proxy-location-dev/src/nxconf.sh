971c21dbe063631874ffd179b5e64f973685cb4e
Aleksander Blaszkiewicz <kqmdjc8@gmail.com>
2023-11-26 12:44:56 +0100

    Add api location

--- reverse-proxy-location-dev/src/nxconf.sh
+++ reverse-proxy-location-dev/src/nxconf.sh
@@ -3,7 +3,8 @@ domain="asddaff.bieda.it"
 out="/etc/nginx/conf.d/default.conf"
 config_file="config" 
 frontend_path="http://192.168.1.103:4003"
-backend_path="http://192.168.1.103:4001"
+redirecting_backend_path="http://192.168.1.103:4001"
+backend_path="http://192.168.1.103:4002"
 
 output_variable=$(cat "$config_file" | tr '\n' '|')
 output_variable=${output_variable%|}
@@ -54,8 +55,26 @@ server {
         proxy_connect_timeout 120;
     }
 
-    location / {
+    location /api/ {
         proxy_pass $backend_path;
+        proxy_set_header Host \$host;
+        proxy_http_version 1.1;
+        proxy_set_header Upgrade \$http_upgrade;
+        proxy_set_header Connection \$connection_upgrade;
+        proxy_ssl_name \$host;
+        proxy_ssl_server_name on;
+        proxy_ssl_verify off;
+        proxy_ssl_protocols  TLSv1 TLSv1.1 TLSv1.2;
+        proxy_ssl_session_reuse off;
+        proxy_set_header X-Forwarded-For \$remote_addr;
+        proxy_set_header X-Forwarded-Proto \$scheme;
+        proxy_read_timeout 120;
+        proxy_send_timeout 120;
+        proxy_connect_timeout 120;
+    }
+
+    location / {
+        proxy_pass $redirecting_backend_path;
         proxy_set_header Host \$host;
         proxy_http_version 1.1;
         proxy_set_header Upgrade \$http_upgrade;
