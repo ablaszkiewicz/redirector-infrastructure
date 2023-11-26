696de9c27a9cc2fc34ff780a9698e2bb8a81c04b
Aleksander Blaszkiewicz <kqmdjc8@gmail.com>
2023-11-26 12:59:09 +0100

    Add prod api rule

--- reverse-proxy-location-prod/src/nxconf.sh
+++ reverse-proxy-location-prod/src/nxconf.sh
@@ -4,6 +4,8 @@ out="/etc/nginx/conf.d/default.conf"
 config_file="config"
 frontend_path="http://192.168.1.103:3003"
 backend_path="http://192.168.1.103:3001"
+backend_path="http://192.168.1.103:3002/"
+
 
 output_variable=$(cat "$config_file" | tr '\n' '|')
 output_variable=${output_variable%|}
@@ -38,6 +40,24 @@ server {
 
     location = / {
         proxy_pass $frontend_path;
+        proxy_set_header Host \$host;
+        proxy_http_version 1.1;
+        proxy_set_header Upgrade \$http_upgrade;
+        proxy_set_header Connection \$connection_upgrade;
+        proxy_ssl_name \$host;
+        proxy_ssl_server_name on;
+        proxy_ssl_verify off;
+        proxy_ssl_protocols  TLSv1 TLSv1.1 TLSv1.2;
+        proxy_ssl_session_reuse off;
+        proxy_set_header X-Forwarded-For \$remote_addr;
+        proxy_set_header X-Forwarded-Proto \$scheme;
+        proxy_read_timeout 120;
+        proxy_send_timeout 120;
+        proxy_connect_timeout 120;
+    }
+
+    location /api/ {
+        proxy_pass $backend_path;
         proxy_set_header Host \$host;
         proxy_http_version 1.1;
         proxy_set_header Upgrade \$http_upgrade;
